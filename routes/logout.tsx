bbf53283de5a3bee40511ee551467a15a4105a15
JohannesSchaefers <42304360+JohannesSchaefers@users.noreply.github.com>
2025-09-15 14:15:54 +0200

    Update logout.tsx

--- routes/logout.tsx
+++ routes/logout.tsx
@@ -1,13 +1,1 @@
-// routes/logout.tsx
-import { Handlers } from "$fresh/server.ts";
 
-export const handler: Handlers = {
-  GET() {
-    // Clear auth cookie
-    const headers = new Headers({
-      Location: "/login",
-      "Set-Cookie": "auth=; Path=/; HttpOnly; SameSite=Strict; Max-Age=0",
-    });
-    return new Response(null, { status: 302, headers });
-  },
-};
