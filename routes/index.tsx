77e0de6a15bd1e079f1b77ee9b12a6b098f04d9b
JohannesSchaefers <42304360+JohannesSchaefers@users.noreply.github.com>
2025-09-15 14:15:13 +0200

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,1 @@
-// routes/index.tsx
-import { Handlers } from "$fresh/server.ts";
 
-export const handler: Handlers = {
-  GET(req) {
-    // Get the full URL from the request
-    const url = new URL(req.url);
-    // Construct the full URL for /login
-    const loginUrl = `${url.origin}/login`;
-    return Response.redirect(loginUrl, 302);
-  },
-};
