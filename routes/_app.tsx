d94989728d2ec0c244ce1b8b518127260f89269e
JohannesSchaefers <42304360+JohannesSchaefers@users.noreply.github.com>
2026-06-28 20:07:39 +0200

    angepasst

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,10 @@
-import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html lang="de">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>2020again</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <title>App</title>
       </head>
       <body>
         <Component />
