360f403e11f772933a986985ac2dbf51b46213cd
JohannesSchaefers <42304360+JohannesSchaefers@users.noreply.github.com>
2026-06-28 18:17:21 +0200

    Login-Inhalt eingefügt

--- routes/login.tsx
+++ routes/login.tsx
@@ -1,1 +1,12 @@
-
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form method="POST" action="/login">
+        <input name="username" />
+        <input name="password" type="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
